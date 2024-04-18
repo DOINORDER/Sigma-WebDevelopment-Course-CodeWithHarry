f94916d8ccdd8fb527737830046f5d5e988d481b
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-19 02:37:35 +0530

    Update script.js

--- vidoe 56/script.js
+++ vidoe 56/script.js
@@ -18,3 +18,26 @@ if ((age+grace)>18){
 else{
     console.log('you cannot drive')
 }
+
+//comment
+/*
+Multi line comment
+ */
+
+//If-Else ladder
+
+let marks = 50;
+if(marks > 33){
+    console.log('you passed')
+}
+else if(marks => 50){
+    console.log('are you kidding me')
+}
+else{
+    console.log("you failed")
+}
+
+
+let a = 6;
+let b = 8
+let c = a>b ? (a - b) : (b - a);
