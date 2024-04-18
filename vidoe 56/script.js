f5c8d450b5e74778a0a5c4fd8ae2fb44505be617
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-19 02:25:00 +0530

    script 56

---
+++ vidoe 56/script.js
@@ -0,0 +1,20 @@
+//Conditional Tutorial
+
+console.log('Hello i am conditional tutorial')
+
+let age = 200;
+
+if (age == 18){
+    console.log('You can drive')
+}
+else{
+    console.log('You cannot drive')
+}
+
+let grace = 2
+if ((age+grace)>18){
+    console.log('You can drive')
+}
+else{
+    console.log('you cannot drive')
+}
