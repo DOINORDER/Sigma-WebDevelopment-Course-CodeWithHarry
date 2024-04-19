99ece277d49f69532eb1ec2c8cd8b00a16c6f57a
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-19 23:05:37 +0530

    script 58

---
+++ video 58/script.js
@@ -0,0 +1,15 @@
+//Functions in JavaScript
+// console.log('Hey sagar, you are awesome my man')
+// console.log('i am a developer')
+
+let name = "sagar";
+
+function nice(name) {
+    console.log('Hey' + name, 'you are awesome')
+    console.log('hey' + name, 'your course is awesome')
+    console.log('hey are you' + name, '?')
+}
+
+let a  = 20
+let b = 80
+console.log(a - b)
