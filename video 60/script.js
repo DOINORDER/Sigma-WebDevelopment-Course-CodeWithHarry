2deebc46204c673f53a7db1b87d7eea9f55a7b54
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-19 23:34:22 +0530

    script 60

---
+++ video 60/script.js
@@ -0,0 +1,25 @@
+//Strings
+
+// >> Write anything inside double quote i.e. " ", it is called STRING.
+//Array type character is known as string
+
+console.log('this is a string tutorial')
+
+let a = 'SagarMalasi' //Sagar is a String here
+console.log(a)
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+
+console.log(a.length)
+
+let friend  = 'Rohan'
+let name = 'Sagar'
+console.log("his name is " + name + " and his friends name is " + friend)
+console.log(`his name is ${name} and his friends name is ${friend}`)
