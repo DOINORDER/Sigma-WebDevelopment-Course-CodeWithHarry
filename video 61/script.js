ee7437b03083abbded4ce690885024c600930b26
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-20 17:26:36 +0530

    script 61

---
+++ video 61/script.js
@@ -0,0 +1,20 @@
+//This is the solution for the vidoe number 59
+let random = Math.random()
+let a = prompt("Enter first number")
+let b = prompt("Enter second number")
+let c = prompt("Enter operation")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/":"**",
+}
+
+if(random > 0.1){
+    alert(` The result is ${eval(`${a} ${c} ${b}`)}`)
+    //perform correct operation
+}
+else{
+c  = obj[c]
+}
