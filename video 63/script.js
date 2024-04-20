a6110e24f1e5d3f55ffcbe2dc73080addc1cbcbb
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-20 17:38:17 +0530

    Update script.js

--- video 63/script.js
+++ video 63/script.js
@@ -3,4 +3,10 @@
 //  >> Arrays is the collection of elements having same data types
 
 let arr = [1, 2, 4, 5 , 7]
+
 console.log(arr)
+console.log(arr.length)
+
+//Array is mutable i.e. we can change value of arrays without forming new arrays while on the otherhand strings are immutable
+
+console.log(arr[0])
