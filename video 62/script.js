6165179f2e8219eee471277704d27c603b32c0e3
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-20 17:33:33 +0530

    script 62

---
+++ video 62/script.js
@@ -0,0 +1,17 @@
+//Business name generator
+
+//Create a business name generator by combining list of adjectives and shop name and another word
+
+//  >>Adjectives : 
+//crazy 
+//amazing
+//fire
+
+// >>Shopname
+//engine
+//foods
+//garments
+
+// >>Another words
+///bros
+//limited
