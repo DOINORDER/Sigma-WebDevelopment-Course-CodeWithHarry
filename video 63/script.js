e7d512420a849f0f4beae0ddadc3afdf3ec2f60d
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-20 17:41:34 +0530

    Update script.js

--- video 63/script.js
+++ video 63/script.js
@@ -3,10 +3,20 @@
 //  >> Arrays is the collection of elements having same data types
 
 let arr = [1, 2, 4, 5 , 7]
-
+//index = 0, 1, 2, 3, 4 , 5
 console.log(arr)
 console.log(arr.length)
 
 //Array is mutable i.e. we can change value of arrays without forming new arrays while on the otherhand strings are immutable
 
 console.log(arr[0])
+console.log(arr[2])
+console.log(arr[4])
+console.log(arr[1])
+
+arr[ 3 ] = 4653
+console.log(arr[3]) //The third index of array will be changed as 4653
+
+arr[ 2 ] = 'Sagar'
+console.log(arr[2])
+console.log(typeof(arr))
