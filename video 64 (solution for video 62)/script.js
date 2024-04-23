2377d13c5209139b0c4e27e62ccb70a51a62b8c2
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-23 22:15:24 +0530

    Update script.js

--- video 64 (solution for video 62)/script.js
+++ video 64 (solution for video 62)/script.js
@@ -1,1 +1,13 @@
 //Solution for Business name generator exercise (Video 62)
+
+let rand = Math.random()
+if(rand < 0.33){
+    //0-0.33, 0.33-0.66, 0.66-1
+    first = "Crazy"
+}
+else if(rand < 0.66 && rand >= 0.33){
+    first = "Amazing"
+}
+else{
+    first = "Fire"
+}
