6b24590769d8ace49bf2097356cb6e7a2a580e18
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-23 22:19:48 +0530

    Update script.js

--- video 64 (solution for video 62)/script.js
+++ video 64 (solution for video 62)/script.js
@@ -1,13 +1,35 @@
 //Solution for Business name generator exercise (Video 62)
 
-let rand = Math.random()
-if(rand < 0.33){
-    //0-0.33, 0.33-0.66, 0.66-1
-    first = "Crazy"
+let rand = Math.random();
+if (rand < 0.33) {
+  //0-0.33, 0.33-0.66, 0.66-1
+
+  //let's generate the first word
+  first = "Crazy";
+} else if (rand < 0.66 && rand >= 0.33) {
+  first = "Amazing";
+} else {
+  first = "Fire";
 }
-else if(rand < 0.66 && rand >= 0.33){
-    first = "Amazing"
+
+if (rand < 0.33) {
+  //0-0.33, 0.33-0.66, 0.66-1
+
+  //let's generate the second word
+  second = "Engine";
+} else if (rand < 0.66 && rand >= 0.33) {
+  second = "Foods";
+} else {
+  second = "Garments";
 }
-else{
-    first = "Fire"
+
+if (rand < 0.33) {
+  //0-0.33, 0.33-0.66, 0.66-1
+
+  //let's generate the third word
+  third = "Bros";
+} else if (rand < 0.66 && rand >= 0.33) {
+  third = "Limited";
+} else {
+  third = "Hub";
 }
