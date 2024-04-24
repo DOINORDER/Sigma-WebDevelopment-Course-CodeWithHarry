d82d9c6a0994f96482c39bb6d7d5df2f52909d25
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-25 00:36:56 +0530

    Create script.js

---
+++ video 69/script.js
@@ -0,0 +1,13 @@
+let a = 6
+
+function factorial(number){
+    let arr = Array.from(Array(number =).keys())
+    console.log(arr)
+    let c = arr.slice(1, ).reduce(a, b) => {
+        return a*b
+    }
+
+    console.log(c)
+}
+
+factorial(a)
