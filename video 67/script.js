2a3f78e7f79c1a9fa8e4d49a54203dc6844bace5
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-24 23:17:08 +0530

    Update script.js

--- video 67/script.js
+++ video 67/script.js
@@ -1,1 +1,8 @@
 //DOM
+
+let a = "hello world"
+console.log(a)
+
+document.body.childNodes[1].childNodes
+
+let cont = document.body.childNodes[1] //ChildNode which is at second place i.e. first index
