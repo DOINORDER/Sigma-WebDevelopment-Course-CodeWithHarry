3ad2729ee04597cccf740e6abe470e7d74658a13
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-25 02:20:44 +0530

    Update script.js

--- video 71/script.js
+++ video 71/script.js
@@ -4,7 +4,7 @@
 let a = document.querySelector(".box").innerHTML = "Hey I am Sagar Malasi"
 console.log(a) 
 
-
+let div = document.createElement("div")
 div.innerHTML = "I have been inserted by <b>Sagar Malasi | Doinorder</b>"
 div.setAttribute("class", "created")
 document.querySelector(".container").append(div)
