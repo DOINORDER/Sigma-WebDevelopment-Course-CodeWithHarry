d5a86e4a59e1af1d9b1a24b4aecf44040b81979c
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-25 00:37:36 +0530

    Update script.js

--- video 69/script.js
+++ video 69/script.js
@@ -1,7 +1,7 @@
 let a = 6
 
 function factorial(number){
-    let arr = Array.from(Array(number =).keys())
+    let arr = Array.from(Array(number + 1).keys())
     console.log(arr)
     let c = arr.slice(1, ).reduce(a, b) => {
         return a*b
