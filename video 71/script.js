8423bbe1e2a9588654aeff73b78d55579807539f
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-25 02:17:22 +0530

    Update script.js

--- video 71/script.js
+++ video 71/script.js
@@ -1,2 +1,5 @@
-let a = document.querySelector(".box").innerText
+// let a = document.querySelector(".box").innerText
+// console.log(a) 
+
+let a = document.querySelector(".box").innerHTML = "Hey I am Sagar Malasi"
 console.log(a) 
