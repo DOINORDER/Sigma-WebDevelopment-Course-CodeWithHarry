fe3aa8e1411d4d656163e6efb38047909f3b648e
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-24 23:25:15 +0530

    Update script.js

--- video 67/script.js
+++ video 67/script.js
@@ -6,3 +6,8 @@ console.log(a)
 document.body.childNodes[1].childNodes
 
 let cont = document.body.childNodes[1] //ChildNode which is at second place i.e. first index
+
+cont.lastElementChild.style.color = "red"
+cont.lastElementChild.style.backgroundColor = "yellow"
+
+
