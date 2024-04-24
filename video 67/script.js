a625360ca9e1501dc388abea4b116ca00ad9ed0e
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-24 23:31:27 +0530

    Update script.js

--- video 67/script.js
+++ video 67/script.js
@@ -1,13 +1,19 @@
 //DOM
 
-let a = "hello world"
-console.log(a)
+let a = "hello world";
+console.log(a);
 
-document.body.childNodes[1].childNodes
+document.body.childNodes[1].childNodes;
 
-let cont = document.body.childNodes[1] //ChildNode which is at second place i.e. first index
+let cont = document.body.childNodes[1]; //ChildNode which is at second place i.e. first index
 
-cont.lastElementChild.style.color = "red"
-cont.lastElementChild.style.backgroundColor = "yellow"
+cont.lastElementChild.style.color = "red";
+cont.lastElementChild.style.backgroundColor = "yellow";
 
+// NodeList(13)[
+//   (text, comment, text, div.box, text, div.box, text, div.box, text, div.box)
+// ];
 
+document.body.firstElementChild;
+document.body.firstElementChild.childNodes;
+document.body.firstElementChild.children;
