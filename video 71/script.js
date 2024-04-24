a37fd7dd22ed06fbd15d09cbbcf824347ef40aca
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-25 02:21:02 +0530

    Update script.js

--- video 71/script.js
+++ video 71/script.js
@@ -1,10 +1,10 @@
 // let a = document.querySelector(".box").innerText
-// console.log(a) 
+// console.log(a)
 
-let a = document.querySelector(".box").innerHTML = "Hey I am Sagar Malasi"
-console.log(a) 
+let a = (document.querySelector(".box").innerHTML = "Hey I am Sagar Malasi");
+console.log(a);
 
-let div = document.createElement("div")
-div.innerHTML = "I have been inserted by <b>Sagar Malasi | Doinorder</b>"
-div.setAttribute("class", "created")
-document.querySelector(".container").append(div)
+let div = document.createElement("div");
+div.innerHTML = "I have been inserted by <b>Sagar Malasi | Doinorder</b>";
+div.setAttribute("class", "created");
+document.querySelector(".container").append(div);
