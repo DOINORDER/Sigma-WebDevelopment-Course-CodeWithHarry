12c32622281d9abc659bf2d83cfa6c409d261d68
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-28 20:40:55 +0530

    index73

---
+++ video 73 (exercise)/script.js
@@ -0,0 +1,5 @@
+function createCard(title, cName, views, monthsOld, duration){
+
+}
+
+createCard("Introduction to Backend | Sigma web development video #2", "CodeWithHarry", 56000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyqpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
