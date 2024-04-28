b296b4b05b49f129d5918040a3b85ed5f6e3fc2d
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-28 20:48:08 +0530

    Update script.js

--- video 74/script.js
+++ video 74/script.js
@@ -1,6 +1,6 @@
 let button = document.getElementById("btn")
 
-btn.addEventListener("click", () => {
+btn.addEventListener("dblclick", () => {
 // document
 alert("Iwasclicked")
 document.querySelector(".box").innerText = "yayyyyyyyyyyyyyyy, you were clicked!!!!"
