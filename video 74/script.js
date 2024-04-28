400ee96d8e8405f8cf45e7ce3e293164a45cf1d1
DOINORDER <117586282+DOINORDER@users.noreply.github.com>
2024-04-28 20:47:42 +0530

    index74

---
+++ video 74/script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+// document
+alert("Iwasclicked")
+document.querySelector(".box").innerText = "yayyyyyyyyyyyyyyy, you were clicked!!!!"
+
+})
